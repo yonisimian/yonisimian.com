f1343fb1c89df8bda7a96d2dc1f454949b116971
yonisimian <yonisimian@email.com>
2023-11-06 14:47:32 +0200

    remove magnifier icon

--- src/components/gallery/GallerySearch.vue
+++ src/components/gallery/GallerySearch.vue
@@ -1,10 +1,5 @@
 <template>
   <div class="flex justify-between gap-2">
-    <!-- <span
-      class="hidden sm:block p-2.5 shadow-sm rounded-xl cursor-pointer"
-    >
-      <i data-feather="search"></i>
-    </span> -->
     <input
       @input="(event) => filterSearch((event.target as HTMLInputElement).value)"
       class="font-general-medium pl-3 pr-1 sm:px-4 py-2 border-1 rounded-lg text-sm sm:text-md"
@@ -19,15 +14,9 @@
 </template>
 
 <script setup lang="ts">
-// import feather from 'feather-icons'
-
 defineProps<{
   filterSearch: (filter: string) => void
 }>()
-
-// onMounted(() => {
-//   feather.replace()
-// })
 </script>
 
 <style scoped></style>
