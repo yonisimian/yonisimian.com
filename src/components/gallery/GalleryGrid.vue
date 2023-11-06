5eaa342c7fe49fa63dd85c458a57fd5d55bf6a52
yonisimian <yonisimian@email.com>
2023-11-06 14:40:05 +0200

    add transition to gallery grid

--- src/components/gallery/GalleryGrid.vue
+++ src/components/gallery/GalleryGrid.vue
@@ -1,6 +1,8 @@
 <template>
   <section class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 mt-6 sm:gap-10">
-    <GalleryCard v-for="(project, index) in projects" :key="index" :project="project" />
+    <TransitionGroup name="slide">
+      <GalleryCard v-for="(project, index) in projects" :key="index" :project="project" />
+    </TransitionGroup>
   </section>
 </template>
 
@@ -12,4 +14,25 @@ defineProps<{
 }>()
 </script>
 
-<style scoped></style>
+<style scoped>
+.slide-enter-from {
+  opacity: 0;
+  rotate: 240deg;
+}
+.slide-enter-to {
+  opacity: 1;
+  translate: 0 0;
+  rotate: 0deg;
+}
+.slide-enter-active,
+.slide-leave-active {
+  transition: all 0.5s;
+}
+.slide-leave-from {
+  opacity: 1;
+}
+.slide-leave-to {
+  opacity: 0;
+  rotate: 240deg;
+}
+</style>
