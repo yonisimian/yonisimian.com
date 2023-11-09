27fd3f00debc2638b699362f930d242ed0930683
yonisimian <yonisimian@email.com>
2023-11-09 12:49:59 +0200

    improve search bar

--- src/components/gallery/GallerySearch.vue
+++ src/components/gallery/GallerySearch.vue
@@ -1,16 +1,17 @@
 <template>
-  <div class="flex justify-between gap-2">
+  <form class="flex justify-between gap-2" novalidate>
     <input
       @input="(event) => filterSearch((event.target as HTMLInputElement).value)"
-      class="pl-3 pr-1 sm:px-4 py-2 min-w-full border-1 border-sky-900 dark:border-gray-200 rounded-full text-sm sm:text-md"
+      class="pl-3 pr-1 sm:px-4 py-2 min-w-full outline-none border-1 border-sky-900 dark:border-gray-200 rounded-full text-sm sm:text-md"
       id="name"
       name="name"
       type="search"
       required="false"
       placeholder="Search Projects"
       aria-label="Name"
+      autocomplete="off"
     />
-  </div>
+  </form>
 </template>
 
 <script setup lang="ts">
@@ -19,4 +20,11 @@ defineProps<{
 }>()
 </script>
 
-<style scoped></style>
+<style scoped>
+input:focus {
+  box-shadow: 0 0 2px #001e24;
+}
+.dark input:focus {
+  box-shadow: 0 0 1px #00d9ff, 0 0 2px #00d9ff, 0 0 3px #00d9ff, 0 0 4px #00d9ff;
+}
+</style>
