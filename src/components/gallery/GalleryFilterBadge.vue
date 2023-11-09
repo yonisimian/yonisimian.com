d79e8d43024a01545d02ccf2a85ae3d833063383
yonisimian <yonisimian@email.com>
2023-11-09 11:29:45 +0200

    add backdrop-filter to badge

--- src/components/gallery/GalleryFilterBadge.vue
+++ src/components/gallery/GalleryFilterBadge.vue
@@ -1,7 +1,7 @@
 <template>
   <div
     @click="toggleCategory"
-    class="select-none px-2 py-2 text-center rounded-full cursor-pointer border-1 border-sky-900 dark:border-[#00d9ff] glow transition-shadow duration-200 ease"
+    class="select-none px-2 py-2 text-center rounded-full cursor-pointer border-1 border-sky-900 dark:border-[#00d9ff] glow backdrop-filter backdrop-blur-[2px] hover:backdrop-blur-[3px] transition-shadow duration-200 ease"
     :class="{
       'text-light-50 bg-sky-900 dark:bg-sky-600': isSelected
     }"
