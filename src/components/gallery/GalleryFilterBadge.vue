5eb7704f9d5deb9d34f40c506add56dbc0c026fc
yonisimian <yonisimian@email.com>
2023-11-09 11:28:01 +0200

    better badge colors for light and dark modes

--- src/components/gallery/GalleryFilterBadge.vue
+++ src/components/gallery/GalleryFilterBadge.vue
@@ -1,9 +1,9 @@
 <template>
   <div
     @click="toggleCategory"
-    class="select-none px-2 py-2 text-center rounded-full cursor-pointer glow transition-shadow duration-200 ease"
+    class="select-none px-2 py-2 text-center rounded-full cursor-pointer border-1 border-sky-900 dark:border-[#00d9ff] glow transition-shadow duration-200 ease"
     :class="{
-      'bg-sky-600': isSelected
+      'text-light-50 bg-sky-900 dark:bg-sky-600': isSelected
     }"
   >
     {{ category }}
@@ -21,11 +21,15 @@ defineProps<{
 </script>
 
 <style scoped>
-.glow {
+.glow:hover {
+  box-shadow: 0 0 2px #001e24;
+}
+
+.dark .glow {
   box-shadow: 0 0 1px #00d9ff, 0 0 2px #00d9ff, 0 0 3px #00d9ff, 0 0 4px #00d9ff;
 }
 
-.glow:hover {
+.dark .glow:hover {
   box-shadow: 0 0 5px #00d9ff, 0 0 6px #00d9ff, 0 0 7px #00d9ff, 0 0 8px #00d9ff;
 }
 </style>
