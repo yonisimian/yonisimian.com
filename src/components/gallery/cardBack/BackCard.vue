3aa4e1ce559fdd03baff8bd920d471b2967a6237
yonisimian <yonisimian@email.com>
2023-11-10 22:42:29 +0200

    appearing animation for BackCard

--- src/components/gallery/cardBack/BackCard.vue
+++ src/components/gallery/cardBack/BackCard.vue
@@ -1,11 +1,51 @@
 <template>
-  <div
-    class="relative flex flex-col justify-start items-center rounded-xl min-h-lg w-[88vw] sm:w-[60vw] bg-sky-100/40 dark:bg-sky-100/10 shadow-md mb-10 sm:mb-0 backdrop-filter backdrop-blur-[12px]"
-  >
-    <slot />
-  </div>
+  <Transition name="slide" appear>
+    <div
+      class="relative flex flex-col justify-start items-center rounded-xl min-h-lg w-[88vw] sm:w-[60vw] bg-sky-50/50 dark:bg-sky-100/10 shadow-md mb-10 sm:mb-0 backdrop-filter backdrop-blur-[12px]"
+    >
+      <slot />
+    </div>
+  </Transition>
 </template>
 
 <script setup lang="ts"></script>
 
-<style scoped></style>
+<style>
+@keyframes slide-in {
+  0% {
+    transform: scale(0);
+  }
+  40% {
+    transform: scale(1.2);
+  }
+  85% {
+    transform: scale(0.95);
+  }
+  100% {
+    transform: scale(1);
+  }
+}
+
+@keyframes slide-out {
+  0% {
+    transform: scale(1);
+  }
+  40% {
+    transform: scale(1.2);
+  }
+  85% {
+    transform: scale(0.9);
+  }
+  100% {
+    transform: scale(0);
+  }
+}
+
+.slide-enter-active {
+  animation: slide-in 0.5s forwards;
+}
+
+.slide-leave-active {
+  animation: slide-out 0.5s forwards;
+}
+</style>
