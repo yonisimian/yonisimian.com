b0cdceaa911e850f7dfc52309c91887ceb028d67
yonisimian <yonisimian@email.com>
2023-11-10 14:41:22 +0200

    add position relative to BackCard

--- src/components/gallery/cardBack/BackCard.vue
+++ src/components/gallery/cardBack/BackCard.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="flex flex-col justify-start items-center rounded-xl min-h-lg w-[88vw] sm:w-[60vw] bg-sky-100/40 dark:bg-sky-100/10 shadow-md mb-10 sm:mb-0 backdrop-filter backdrop-blur-[12px]"
+    class="relative flex flex-col justify-start items-center rounded-xl min-h-lg w-[88vw] sm:w-[60vw] bg-sky-100/40 dark:bg-sky-100/10 shadow-md mb-10 sm:mb-0 backdrop-filter backdrop-blur-[12px]"
   >
     <slot />
   </div>
