8a90bd831e6be56ba87f55807329f393ab22236a
yonisimian <yonisimian@email.com>
2023-11-10 22:40:59 +0200

    cleaning BackBackground classes

--- src/components/gallery/cardBack/BackBackground.vue
+++ src/components/gallery/cardBack/BackBackground.vue
@@ -1,22 +1,11 @@
 <template>
-  <div class="background flex justify-center items-center">
+  <div
+    class="fixed top-0 left-0 z-10 w-screen h-screen overflow-hidden select-none flex justify-center items-center"
+  >
     <slot />
   </div>
 </template>
 
 <script setup lang="ts"></script>
 
-<style scoped>
-.background {
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100vw;
-  height: 100vh;
-  overflow: hidden;
-  user-select: none;
-
-  background-color: rgba(0, 0, 0, 0.3);
-  z-index: 10;
-}
-</style>
+<style scoped></style>
