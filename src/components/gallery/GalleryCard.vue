390aaf940c68f1f375d6c23bc866b4b2c3d833a6
yonisimian <yonisimian@email.com>
2023-11-10 14:38:26 +0200

    remove flip stuff from GalleryCard

--- src/components/gallery/GalleryCard.vue
+++ src/components/gallery/GalleryCard.vue
@@ -1,15 +1,4 @@
 <template>
-  <!-- <div class="flip-card">
-    <div class="flip-card-inner">
-      <div class="flip-card-front">
-        <GalleryCardFront :project="project" />
-      </div>
-      <div class="flip-card-back">
-        <GalleryCardBack :project="project" />
-      </div>
-    </div>
-  </div> -->
-
   <div>
     <GalleryCardBack v-if="isChosen" :discard="() => (isChosen = false)" :project="project" />
     <GalleryCardFront @click="isChosen = true" :project="project" />
@@ -24,39 +13,7 @@ defineProps<{
   project: Project
 }>()
 
-// @ts-ignore
 const isChosen = ref(false)
 </script>
 
-<style scoped>
-.flip-card {
-  background-color: transparent;
-  height: 100%;
-  perspective: 1000px;
-}
-
-.flip-card-inner {
-  position: relative;
-  width: 100%;
-  height: 100%;
-  transition: transform 0.8s;
-  transform-style: preserve-3d;
-}
-
-.flip-card:hover .flip-card-inner {
-  transform: rotateY(180deg);
-}
-
-.flip-card-front,
-.flip-card-back {
-  position: absolute;
-  width: 100%;
-  height: 100%;
-  -webkit-backface-visibility: hidden;
-  backface-visibility: hidden;
-}
-
-.flip-card-back {
-  transform: rotateY(180deg);
-}
-</style>
+<style scoped></style>
