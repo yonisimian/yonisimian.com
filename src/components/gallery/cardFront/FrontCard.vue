7e829b78131d137bf4de0f3e869d9c92e2d7dc29
yonisimian <yonisimian@email.com>
2023-11-10 22:41:32 +0200

    add select-none to FrontCard

--- src/components/gallery/cardFront/FrontCard.vue
+++ src/components/gallery/cardFront/FrontCard.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="grid grid-rows-3 rounded-xl bg-sky-400/5 shadow-lg hover:shadow-xl cursor-pointer mb-10 sm:mb-0 backdrop-filter backdrop-blur-[3px] hover:backdrop-blur-[4px] transform transition-transform transition-shadow transition-opacity transition-colors duration-500 hover:scale-107 dark:bg-ternary-dark"
+    class="grid grid-rows-3 rounded-xl bg-sky-400/5 shadow-lg hover:shadow-xl cursor-pointer select-none mb-10 sm:mb-0 backdrop-filter backdrop-blur-[3px] hover:backdrop-blur-[4px] transform transition-transform transition-shadow transition-opacity transition-colors duration-500 hover:scale-107 dark:bg-ternary-dark"
   >
     <slot />
   </div>
