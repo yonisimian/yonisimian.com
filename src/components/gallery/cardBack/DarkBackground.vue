621613dfdda6d4d97be9ba6ec362061a57655588
yonisimian <yonisimian@email.com>
2023-11-10 22:41:13 +0200

    creating a DarkBackground component

---
+++ src/components/gallery/cardBack/DarkBackground.vue
@@ -0,0 +1,27 @@
+<template>
+  <Transition name="fade" appear>
+    <div
+      class="bg-black bg-opacity-30 fixed top-0 left-0 z-10 w-screen h-screen overflow-hidden select-none"
+    />
+  </Transition>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+.fade-enter-active,
+.fade-leave-active,
+.fade-appear-active {
+  transition: opacity 0.5s;
+}
+.fade-enter-from,
+.fade-leave-to,
+.fade-appear-from {
+  opacity: 0;
+}
+.fade-enter-to,
+.fade-leave-from,
+.fade-appear-to {
+  opacity: 1;
+}
+</style>
