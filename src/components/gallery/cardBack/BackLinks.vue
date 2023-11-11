a7364784e73d81a16ba00fb9173ec00fe010b926
yonisimian <yonisimian@email.com>
2023-11-11 17:22:13 +0200

    remove <a> style from BackLinks

--- src/components/gallery/cardBack/BackLinks.vue
+++ src/components/gallery/cardBack/BackLinks.vue
@@ -2,7 +2,7 @@
   <p class="pt-1 pb-3 mt-auto text-left w-full max-w-4xl">
     links:
     <span v-for="(link, index) in links" :key="index">
-      <a :href="link.url" target="_blank" rel="noopener" class="text-blue-500 hover:text-blue-700">
+      <a :href="link.url" target="_blank" rel="noopener">
         {{ link.title }}
       </a>
       <span v-if="index < links.length - 1">, </span>
