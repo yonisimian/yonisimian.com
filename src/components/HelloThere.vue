c1ecb6202796e87737232aafa5ca0786aaf932a6
yonisimian <yonisimian@email.com>
2023-11-11 18:57:05 +0200

    update HelloThere

--- src/components/HelloThere.vue
+++ src/components/HelloThere.vue
@@ -1,11 +1,22 @@
 <template>
   <header class="text-center text-md">
-    <h1 class="font-serif font-bold text-4xl mb-8">Hello 👋 there, welcome my portfolio!</h1>
+    <h1 class="font-serif font-bold text-4xl mb-8">Welcome to my portfolio! 👋</h1>
 
-    <p>Maybe write a bit about myself? Maybe add some <a href="#">links</a> as well?</p>
+    <p>
+      My name is <strong>Yehonatan Simian</strong>, and this is the portfolio I made for Master of
+      Science in Music Technology application at Georgia Tech School of Music.
+    </p>
+    <p>
+      Please enjoy this showcase of my key skills, competencies, and experience in music, computer
+      science and academic research 😊.
+    </p>
   </header>
 </template>
 
 <script setup lang="ts"></script>
 
-<style scoped></style>
+<style scoped>
+strong {
+  color: #00b3ff;
+}
+</style>
