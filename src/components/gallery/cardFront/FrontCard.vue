15766e93be60dda12049c8c985399cc3d8ae0e12
yonisimian <yonisimian@email.com>
2023-11-11 18:38:55 +0200

    front card glowing light perimeter effect

--- src/components/gallery/cardFront/FrontCard.vue
+++ src/components/gallery/cardFront/FrontCard.vue
@@ -8,4 +8,50 @@
 
 <script setup lang="ts"></script>
 
-<style scoped></style>
+<style scoped>
+.grid {
+  position: relative;
+  overflow: hidden;
+}
+
+.dark .grid::before {
+  content: '';
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 10px;
+  height: 10px;
+  background: #00b3ff;
+  box-shadow: 0 0 5px #00b3ff, 0 0 10px #00b3ff, 0 0 15px #00b3ff, 0 0 20px #00b3ff;
+  border-radius: 50%;
+  animation: glow 3s infinite linear;
+  visibility: hidden;
+}
+
+.grid:hover::before {
+  visibility: visible;
+}
+
+@keyframes glow {
+  0% {
+    top: -10px;
+    left: -20px;
+  }
+  25% {
+    top: -10px;
+    left: 100%;
+  }
+  50% {
+    top: 100%;
+    left: 100%;
+  }
+  75% {
+    top: 100%;
+    left: -10px;
+  }
+  100% {
+    top: 0;
+    left: -10px;
+  }
+}
+</style>
