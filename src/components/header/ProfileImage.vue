3d114ad9b3193c2b9ec66f2d03f6fa1dc774986a
yonisimian <yonisimian@email.com>
2023-11-14 03:15:45 +0200

    add glow to dark ProfileImage

--- src/components/header/ProfileImage.vue
+++ src/components/header/ProfileImage.vue
@@ -3,11 +3,15 @@
     <img
       src="/@/assets/pp.png"
       alt="Yehonatan Simian"
-      class="rounded-full aspect-w-1 aspect-h-1 h-[40vh] mb-16 m-auto"
+      class="rounded-full aspect-w-1 aspect-h-1 h-[40vh] mb-16 m-auto transition-shadow"
     />
   </div>
 </template>
 
 <script setup lang="ts"></script>
 
-<style scoped></style>
+<style scoped>
+.dark img {
+  box-shadow: 0 0 8px #00b3ff;
+}
+</style>
