c3c1297a5eaf96d65cf28e9001116f63d784f2ec
yonisimian <yonisimian@email.com>
2023-11-14 02:52:27 +0200

    Create a reusable DarkTheme component

--- src/components/Footer/icons/DarkThemeIcon.vue
+++ src/components/Footer/icons/DarkThemeIcon.vue
@@ -1,23 +1,10 @@
 <template>
   <li class="cursor-pointer text-2xl">
-    <a @click="toggleDark()" class="text-cyan-700 hover:text-cyan-500" :title="'Toggle Theme'">
-      <i i="dark:ph-sun ph-moon" />
-    </a>
+    <DarkTheme />
   </li>
 </template>
 
-<script setup lang="ts">
-import { ref } from 'vue'
-import { useTheme } from '/@/composables'
-
-const { toggleDark } = useTheme()
-
-const show = ref(false)
-
-setTimeout(() => {
-  show.value = true
-}, 1000)
-</script>
+<script setup lang="ts"></script>
 
 <style scoped>
 .footer-link {
---
+++ src/components/header/DarkTheme.vue
@@ -0,0 +1,20 @@
+<template>
+  <button @click="toggleDark()" class="text-cyan-700 hover:text-cyan-500" :title="'Toggle Theme'">
+    <i i="dark:ph-sun ph-moon" />
+  </button>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import { useTheme } from '/@/composables'
+
+const { toggleDark } = useTheme()
+
+const show = ref(false)
+
+setTimeout(() => {
+  show.value = true
+}, 1000)
+</script>
+
+<style scoped></style>
