48727b4c6427c914b89fa4359b83d811d5cdd945
yonisimian <yonisimian@email.com>
2023-12-10 10:11:54 +0200

    add min-h to BackEmbed

--- src/components/gallery/cardBack/BackEmbed.vue
+++ src/components/gallery/cardBack/BackEmbed.vue
@@ -1,5 +1,5 @@
 <template>
-  <iframe class="py-4 w-full h-full" :src="link" allowfullscreen></iframe>
+  <iframe class="py-4 w-full min-h-xs xl:min-h-sm h-full" :src="link" allowfullscreen></iframe>
 </template>
 
 <script setup lang="ts">
