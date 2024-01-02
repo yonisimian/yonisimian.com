cbee968cb927239b495d8c3413aae5983e467d33
yonisimian <yonisimian@email.com>
2024-01-02 21:19:04 +0200

    smaller year text size

--- src/components/gallery/cardFront/FrontYear.vue
+++ src/components/gallery/cardFront/FrontYear.vue
@@ -1,6 +1,6 @@
 <template>
   <div v-if="year" class="absolute mt-2 left-1/2 transform -translate-x-1/2">
-    <p class="font-general-medium text-lg text-ternary-dark dark:text-ternary-light">
+    <p class="font-general-medium opacity-75 text-ternary-dark dark:text-ternary-light">
       {{ year }}
     </p>
   </div>
