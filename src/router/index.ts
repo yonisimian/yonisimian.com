ff53962fea08cf40704a76f1fbcaea61aa4b782d
yonisimian <yonisimian@email.com>
2025-06-22 08:26:57 +0300

    add default trip path

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import { encodeURIStep, steps } from '/@/data/trip'
 
 import PortfolioView from '../views/PortfolioView.vue'
 import TheBigTripView from '../views/TheBigTripView.vue'
 
+const defaultTripPath = `/thebigtrip/${encodeURIStep(steps[0])}`
+
 const routes = [
   { path: '/', redirect: '/portfolio' },
   { path: '/portfolio', component: PortfolioView },
+  { path: '/thebigtrip', redirect: () => defaultTripPath },
   {
     path: '/thebigtrip/:id',
     component: TheBigTripView
