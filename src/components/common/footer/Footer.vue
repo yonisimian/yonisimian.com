4a6fd571805ad266224807b73a52827b5f5f3de0
yonisimian <yonisimian@email.com>
2025-06-23 14:26:25 +0300

    unite the two sites (complete)

--- src/components/common/footer/Footer.vue
+++ src/components/common/footer/Footer.vue
@@ -1,9 +1,18 @@
 <template>
-  <footer class="mb-8 text-center">
+  <footer class="flex flex-col text-center gap-3 mb-8">
+    <hr class="w-xl max-w-[82vw] mx-auto bg-blue-gray-200 dark:bg-blue-gray-800 h-[2px] border-0" />
     <FooterIcons />
 
-    <span class="text-xs"
-      >Made by
+    <nav class="flex flex-row justify-center gap-2 text-xs">
+      <RouterLink to="/">Home</RouterLink>
+      <p>•</p>
+      <RouterLink :to="'/' + PortfolioRoute">My Portfolio</RouterLink>
+      <p>•</p>
+      <RouterLink :to="'/' + TripRoute">The Big Trip</RouterLink>
+    </nav>
+
+    <span class="text-xs">
+      Made by
       <a
         class="opacity-80 text-cyan-500 hover:text-cyan-400"
         href="https://github.com/yonisimian"
@@ -13,12 +22,6 @@
         Yehonatan Simian
       </a>
     </span>
-
-    <nav class="flex flex-col">
-      <RouterLink to="/">Go to Home</RouterLink>
-      <RouterLink :to="'/' + PortfolioRoute">My Portfolio</RouterLink>
-      <RouterLink :to="'/' + TripRoute">The Big Trip</RouterLink>
-    </nav>
   </footer>
 </template>
 
--- src/components/common/footer/FooterIcons.vue
+++ src/components/common/footer/FooterIcons.vue
@@ -1,5 +1,5 @@
 <template>
-  <ul class="flex justify-between w-5/6 max-w-72 mx-auto mb-8">
+  <ul class="flex justify-between w-5/6 max-w-72 mx-auto">
     <DarkThemeIcon />
     <YoutubeIcon />
     <GitHubIcon />
