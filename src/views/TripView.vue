aa05f84b632a646a81109d3f790aa65ed19f9243
yonisimian <yonisimian@email.com>
2025-06-23 21:27:45 +0300

    remove continents destbar :(

--- src/views/TripView.vue
+++ src/views/TripView.vue
@@ -2,8 +2,8 @@
   <div class="flex flex-col items-center justify-center mx-auto max-w-4xl">
     <TripHeader />
 
-    <article class="flex flex-col items-center justify-center w-full">
-      <DestBar :destinations="continents" :currDest="currContinent" :chooseDest="chooseContinent" />
+    <article class="flex flex-col items-center justify-center w-full gap-4">
+      <!-- <DestBar :destinations="continents" :currDest="currContinent" :chooseDest="chooseContinent" /> -->
       <DestBar :destinations="countries" :currDest="currCountry" :chooseDest="chooseCountry" />
       <DestBar :destinations="steps" :dates :currDest="currStep" :chooseDest="chooseStep" />
     </article>
@@ -17,10 +17,10 @@
 </template>
 
 <script setup lang="ts">
-import { continents, countries, steps, dates } from '/@/data/trip'
+import { /*continents,*/ countries, steps, dates } from '/@/data/trip'
 import { useTripState } from '/@/composables/useTripState'
 
-const { currStep, currCountry, currContinent, chooseStep, chooseCountry, chooseContinent } =
+const { currStep, currCountry, chooseStep, chooseCountry /*, currContinent, chooseContinent*/ } =
   useTripState()
 </script>
 
