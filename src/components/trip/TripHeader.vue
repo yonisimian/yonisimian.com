5cdda61b72ccbb44a5e825514c0f02d2662165f1
yonisimian <yonisimian@email.com>
2025-06-28 21:59:32 +0300

    create cute collections tada animation

--- src/components/trip/TripHeader.vue
+++ src/components/trip/TripHeader.vue
@@ -1,30 +1,57 @@
 <template>
   <header class="flex flex-col items-center justify-center gap-2 py-4 w-full select-none">
-    <!-- <h1 class="text-4xl font-bold">The Big Trip</h1> -->
     <TitleSeeThrough class="text-4xl" />
-    <!-- <h2
-      class="text-md text-center -mt-5 px-10 py-0 text-blue-gray-900"
-      style="background: linear-gradient(to right, transparent, white 15%, white 85%, transparent)"
-    >
-      Explore the World through My Eyes
-    </h2> -->
     <div
       class="w-full flex items-center justify-center text-sm text-blue-gray-800 dark:text-blue-gray-300"
     >
-      <h3 @click="collection = CollectionType.Highlights">highlights</h3>
+      <h3
+        :class="{ 'animate-my-tada': bounceIndex === 5 }"
+        @click="collection = CollectionType.Highlights"
+      >
+        highlights
+      </h3>
       <p class="mx-2">•</p>
-      <h3 @click="collection = CollectionType.Music">music</h3>
+      <h3
+        :class="{ 'animate-my-tada': bounceIndex === 6 }"
+        @click="collection = CollectionType.Music"
+      >
+        music
+      </h3>
       <p class="mx-2">•</p>
-      <h3 @click="collection = CollectionType.People">people</h3>
+      <h3
+        :class="{ 'animate-my-tada': bounceIndex === 7 }"
+        @click="collection = CollectionType.People"
+      >
+        people
+      </h3>
     </div>
   </header>
 </template>
 
 <script setup lang="ts">
+import { ref, onMounted, onUnmounted } from 'vue'
 import { CollectionType } from '/@/types/trip'
 import { useTripState } from '/@/composables/useTripState'
 
 const { collection } = useTripState()
+
+const bounceIndex = ref(-1)
+let interval: number | undefined
+
+onMounted(() => {
+  interval = window.setInterval(() => {
+    bounceIndex.value = (bounceIndex.value + 1) % 8 // cycle through 0, 1, 2... 7
+  }, 1500)
+})
+
+// only do the animation when static
+window.addEventListener('scroll', () => {
+  bounceIndex.value = -1
+})
+
+onUnmounted(() => {
+  if (interval) clearInterval(interval)
+})
 </script>
 
 <style scoped>
@@ -33,5 +60,30 @@ h3 {
 }
 h3:hover {
   color: #00b3ff; /* same as strong */
+}
+@keyframes my-tada {
+  from {
+    transform: scale3d(1, 1, 1);
+  }
+  10%,
+  20% {
+    transform: scale3d(0.9, 0.9, 0.9) rotate3d(0, 0, 1, -3deg);
+  }
+  30%,
+  50%,
+  70% {
+    transform: scale3d(1.2, 1.2, 1.2) rotate3d(0, 0, 1, 3deg);
+  }
+  40%,
+  60%,
+  80% {
+    transform: scale3d(1.2, 1.2, 1.2) rotate3d(0, 0, 1, -3deg);
+  }
+  to {
+    transform: scale3d(1, 1, 1);
+  }
+}
+.animate-my-tada {
+  animation: my-tada 1s linear 1;
 }
 </style>
