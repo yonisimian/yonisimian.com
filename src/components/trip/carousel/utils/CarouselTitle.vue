0fb08e978bfccc8dcb25e2df7a5ba6b09050ce90
yonisimian <yonisimian@email.com>
2025-07-05 18:52:15 +0300

    make title glow more gentle

--- src/components/trip/carousel/utils/CarouselTitle.vue
+++ src/components/trip/carousel/utils/CarouselTitle.vue
@@ -12,7 +12,7 @@ const { activeCollection } = useTripState()
 
 <style scoped>
 h2 {
-  text-shadow: 0 0 1px #fff, 0 0 2px #fff, 0 0 3px #e60073, 0 0 4px #e60073, 0 0 5px #e60073,
-    0 0 6px #e60073, 0 0 7px #e60073;
+  text-shadow: 0 0 1px #fff, 0 0 2px #fff, 0 0 3px #e60073, 0 0 4px #e6007390, 0 0 5px #e6007390,
+    0 0 6px #e6007390;
 }
 </style>
