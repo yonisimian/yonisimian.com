b462f0508ee9247d22196fc9105ef6e9272a492c
yonisimian <yonisimian@email.com>
2025-07-15 20:27:36 +0300

    StepNavigationButtons always have two child divs

--- src/components/trip/step/StepNavigationButtons.vue
+++ src/components/trip/step/StepNavigationButtons.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="flex items-center justify-between w-full z-5">
-    <a v-if="prevStep" href="#" @click.prevent="choosePrevStep">← {{ prevStep.shortName }}</a>
-    <a v-if="nextStep" href="#" @click.prevent="chooseNextStep">{{ nextStep.shortName }} →</a>
+    <div>
+      <a v-if="prevStep" href="#" @click.prevent="choosePrevStep">← {{ prevStep.shortName }}</a>
+    </div>
+    <div>
+      <a v-if="nextStep" href="#" @click.prevent="chooseNextStep">{{ nextStep.shortName }} →</a>
+    </div>
   </div>
 </template>
 
