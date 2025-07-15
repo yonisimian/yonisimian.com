ef22607663883b701ab487c7d5f0f077064676b3
yonisimian <yonisimian@email.com>
2025-07-15 23:40:58 +0300

    add shadow to make CarouselTitle more readable

--- src/components/trip/carousel/utils/CarouselTitle.vue
+++ src/components/trip/carousel/utils/CarouselTitle.vue
@@ -13,6 +13,6 @@ const { activeCollection } = useTripState()
 <style scoped>
 h2 {
   text-shadow: 0 0 1px #fff, 0 0 2px #fff, 0 0 3px #e60073, 0 0 4px #e6007390, 0 0 5px #e6007390,
-    0 0 6px #e6007390;
+    0 0 6px #e6007390, 1px 1px 0 #000000;
 }
 </style>
