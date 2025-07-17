f22b9cec4c89750e3421a83197444d4edaa89ce4
yonisimian <yonisimian@email.com>
2025-07-17 06:45:54 +0300

    remove CarouselGalleryWrapper

--- src/components/trip/carousel/MediaCarousel.vue
+++ src/components/trip/carousel/MediaCarousel.vue
@@ -1,6 +1,10 @@
 <template>
   <CarouselFullscreenWrapper>
-    <CarouselGalleryWrapper />
+    <div v-if="activeCollection.stepslides.length" class="w-full h-full">
+      <CarouselGallery />
+      <CarouselThumbnail v-if="!fullscreen" />
+    </div>
+    <CarouselEmpty v-else />
   </CarouselFullscreenWrapper>
 </template>
 
@@ -9,7 +13,7 @@ import 'vue3-carousel/carousel.css'
 import { watch } from 'vue'
 import { useTripState } from '/@/composables/useTripState'
 
-const { currStep, slide, fullscreen, collection } = useTripState()
+const { currStep, slide, fullscreen, collection, activeCollection } = useTripState()
 
 // reset slide when step changes or entering custom slides mode
 watch([currStep, collection], () => (slide.value = 0))
