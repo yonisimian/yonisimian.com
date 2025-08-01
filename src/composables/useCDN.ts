4015b5f451e6f93bdc5894935ff99844ef981f82
yonisimian <yonisimian@email.com>
2025-08-01 15:57:01 +0300

    fck cdn smh

--- src/composables/useCDN.ts
+++ src/composables/useCDN.ts
@@ -1,8 +1,14 @@
-export const useMediaCdn = (path: string, options: { w: number; fm: string }) => {
-  const base = path.startsWith('/') ? path.slice(1) : path
+export const useMediaCdn = (path: string, _options: { w: number; fm: string }) => {
+  const base = path.startsWith('/media/')
+    ? path.slice(7)
+    : path.startsWith('/')
+    ? path.slice(1)
+    : path
 
-  const width = options.w
-  const format = options.fm
+  // const width = options.w
+  // const format = options.fm
 
-  return `/cdnimg/${base}?w=${width}&fm=${format}`
+  // return `/cdnimg/${base}?w=${width}&fm=${format}`
+
+  return `/media/${base}` // honestly, it's works better than the CDN
 }
