d10adc4aa1201e156bb83624eb0dccf6b4fad39f
yonisimian <yonisimian@email.com>
2025-08-03 03:30:02 +0300

    enhance ToolbarIcon with defaults

--- src/components/trip/carousel/utils/toolbar/ToolbarIcon.vue
+++ src/components/trip/carousel/utils/toolbar/ToolbarIcon.vue
@@ -3,33 +3,37 @@
     class="inline-flex text-white bg-opacity-30 border-none p-2"
     :class="{
       ' hover:bg-white hover:bg-opacity-30': !disabled,
-      [buttonBgColor ?? 'bg-black']: true
+      [buttonBgColor]: true
     }"
     @click="onClick"
     :title
-    :disabled="disabled === undefined ? false : disabled"
+    :disabled
   >
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-      :fill="fill ?? 'currentColor'"
-      :stroke="stroke ?? 'currentColor'"
-      class="w-6 h-6"
-    >
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" :fill :stroke :class="svgClass">
       <slot />
     </svg>
   </button>
 </template>
 
 <script setup lang="ts">
-defineProps<{
-  fill?: string
-  stroke?: string
-  title?: string
-  disabled?: boolean
-  buttonBgColor?: string
-  onClick?: () => void
-}>()
+withDefaults(
+  defineProps<{
+    fill?: string
+    stroke?: string
+    title?: string
+    disabled?: boolean
+    buttonBgColor?: string
+    svgClass?: string
+    onClick?: () => void
+  }>(),
+  {
+    fill: 'currentColor',
+    stroke: 'currentColor',
+    disabled: false,
+    buttonBgColor: 'bg-black',
+    svgClass: 'w-6 h-6'
+  }
+)
 </script>
 
 <style scoped></style>
