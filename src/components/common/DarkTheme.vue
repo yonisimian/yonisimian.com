3e7dbf8acbdadf3704810ea703b425ebb4af645e
yonisimian <yonisimian@email.com>
2025-08-08 06:42:43 +0300

    remove unused ref

--- src/components/common/DarkTheme.vue
+++ src/components/common/DarkTheme.vue
@@ -1,20 +1,13 @@
 <template>
-  <button @click="toggleDark()" class="text-cyan-700 hover:text-cyan-500" :title="'Toggle Theme'">
+  <button @click="toggleDark()" class="text-cyan-700 hover:text-cyan-500" title="Toggle Theme">
     <i i="dark:ph-sun ph-moon" />
   </button>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
 import { useTheme } from '/@/composables'
 
 const { toggleDark } = useTheme()
-
-const show = ref(false)
-
-setTimeout(() => {
-  show.value = true
-}, 1000)
 </script>
 
 <style scoped></style>
