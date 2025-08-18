99cb3aac8a841bda29c0e7885abdd6d41411e1a7
yonisimian <yonisimian@email.com>
2025-08-18 10:57:56 +0300

    remove ricktroll autoplay

--- src/components/common/RickTroll.vue
+++ src/components/common/RickTroll.vue
@@ -2,7 +2,7 @@
   <div v-show="visible">
     <YDarkBackground />
     <div class="fixed top-0 left-0 w-screen h-screen z-9999 flex justify-center items-center">
-      <video ref="video" class="max-w-full max-h-full" autoplay @ended="visible = false">
+      <video ref="video" class="max-w-full max-h-full" @ended="visible = false">
         <source src="/media/RickTroll.webm" type="video/webm" />
         Your browser does not support the video tag.
       </video>
