cb23d42fa7239d5416591f31a33d2ea01fef8ec9
yonisimian <yonisimian@email.com>
2025-08-19 19:57:13 +0300

    remove jordi

--- src/components/common/footer/Footer.vue
+++ src/components/common/footer/Footer.vue
@@ -13,8 +13,6 @@
       <RouterLink :to="'/' + CookbookRoute">Michal's Cookbook</RouterLink>
       <p class="hidden sm:block">•</p>
       <RouterLink :to="'/' + VegetavailableRoute">Mom's Vegetables</RouterLink>
-      <p class="hidden sm:block">•</p>
-      <RouterLink :to="'/' + JordiMonitorRoute">Ronen's Monitor</RouterLink>
     </nav>
 
     <span class="text-xs">
@@ -32,13 +30,7 @@
 </template>
 
 <script setup lang="ts">
-import {
-  TripRoute,
-  PortfolioRoute,
-  CookbookRoute,
-  VegetavailableRoute,
-  JordiMonitorRoute
-} from '/@/data/globals'
+import { TripRoute, PortfolioRoute, CookbookRoute, VegetavailableRoute } from '/@/data/globals'
 </script>
 
 <style scoped></style>
